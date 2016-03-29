e000a90e1a7503fc7e2310a84fea14c643929008
LisaMabley <lisamabley@gmail.com>
2016-03-29 17:03:05 -0500

    teamwork tues

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
+
+// Initialize variables
+var applePrice = 0.00;
+var orangePrice = 0.00;
+var bananaPrice = 0.00;
+var grapesPrice = 0.00;
+
+var numApples = 0;
+var numOranges = 0;
+var numBananas = 0;
+var numGrapes = 0;
+
+var playerBalance = 100.00;
+
+// Initialize functions
+function buy(fruit) {
+	playerBalance -= fruit.price;
+	fruit.playerInventory ++;
+}
+
+function sell(fruit) {
+	playerBalance += fruit.price;
+	fruit.playerInventory --;
+}
+
+function adjustPrice() {
+
+}
